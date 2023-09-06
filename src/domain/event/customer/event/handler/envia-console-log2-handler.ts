14d4510ddc7fd95a4d318ecd90583e71fac41276
Paulo Ricardo <60313109+paulogaviao@users.noreply.github.com>
2023-09-06 11:36:22 -0300

    Update envia-console-log2-handler.ts

--- src/domain/event/customer/event/handler/envia-console-log2-handler.ts
+++ src/domain/event/customer/event/handler/envia-console-log2-handler.ts
@@ -5,7 +5,7 @@ import CustomerCreatedEvent from "../../../customer/event/customer-created.event
 export default class EnviaConsoleLog2Handler implements EventHandlerInterface<CustomerCreatedEvent> {
 
     handle(event: EventoInterface): void {
-        console.log("Esse é o segundo console.log do evento: CustomerCreated")
+       console.log("Esse é o segundo console.log do evento: CustomerCreated" , JSON.stringify(event.eventData, null, 2))
     }
     
 }
