fd17f5aad99a66f74fa60d1a684e313eb51503b4
Paulo Ricardo <60313109+paulogaviao@users.noreply.github.com>
2023-09-06 11:32:03 -0300

    Update event-envia.interface.ts

--- src/domain/event/customer/event/handler/event-envia.interface.ts
+++ src/domain/event/customer/event/handler/event-envia.interface.ts
@@ -1,7 +1,9 @@
 import EventHandlerInterface from "../../../@shared/event-handler.interface";
 import CustomerCreatedEvent from "../customer-created.event";
+import CustomerUpdateEvent from "../customer-update.event";
 
 export default interface EventEnviaInterface{
     notifica(event: CustomerCreatedEvent):void;
     cria(eventName: string, eventHandler: EventHandlerInterface):void;
+    altera(eventName: string, eventHandler: EventHandlerInterface, eventHandlerUp: EventHandlerInterface): void;
 }
